9067a21d85e2166bc199dea09a37943dcc511e1e
twodog1953 <songyuchen1997@gmail.com>
2024-05-22 12:52:15 -0700

    Trying the github commit function. Imported some modules.

---
+++ kaggle_ml.py
@@ -0,0 +1,6 @@
+# mostly the code from Kaggle ML class
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
