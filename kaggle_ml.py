968530eb8d412c3f8a57dcbb6bf94701e0f903ee
twodog1953 <69299574+twodog1953@users.noreply.github.com>
2024-05-22 13:16:27 -0700

    Update kaggle_ml.py

--- kaggle_ml.py
+++ kaggle_ml.py
@@ -4,3 +4,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder
+
+# all functions
